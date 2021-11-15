1dd41cf1527a99109efc42710f5b304ffaa00995
WilliamBritoFC <william.brito@fcamara.com.br>
2021-11-15 17:31:06 -0300

    completed

--- src/index.js
+++ src/index.js
@@ -23,17 +23,23 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
+  }
+
+  if(updatedRepository.likes) {
+    delete updatedRepository.likes;
   }
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
@@ -48,7 +54,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +72,10 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  const repository = repositories[repositoryIndex];
+  repository.likes++;
 
-  return response.json('likes');
+  return response.json(repository);
 });
 
 module.exports = app;
